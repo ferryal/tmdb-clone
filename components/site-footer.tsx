a179e42183bfcbd62ce71cc91af854b69fac6b72
Ferryal Fajar <ferryal21@gmail.com>
2024-08-31 14:27:05 +0700

    feat: add component nav, header, footer

---
+++ components/site-footer.tsx
@@ -0,0 +1,36 @@
+import { ComponentProps } from "react"
+import Link from "next/link"
+import { navigation } from "@/config"
+
+import { Icons } from "@/components/icons"
+
+export const SiteFooter: React.FC<ComponentProps<"footer">> = () => {
+  const year = new Date().getFullYear()
+
+  return (
+    <footer className="relative mt-12 border-t bg-background">
+      <div className="container flex flex-col lg:flex-row">
+        <div className="px-2 py-8 pt-12 text-muted-foreground md:p-12">
+          <Icons.Tmdb className="size-12" color="blue" />
+        </div>
+
+        <div className="flex-1 p-12 px-2 py-8 md:p-12">
+          <div className="hidden md:flex">
+            {navigation.items.slice(1, navigation.items.length).map((item) => (
+              <ul className="flex-1" key={item.title}>
+                <p className="mb-4 text-muted-foreground">{item.title}</p>
+                {item.items?.map((subitem) => (
+                  <li className="mb-2 text-sm" key={subitem.href}>
+                    <Link href={subitem.href} prefetch={false}>
+                      {subitem.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
---
+++ components/site-menu.tsx
@@ -0,0 +1,97 @@
+"use client"
+
+import Link from "next/link"
+import { NavItem, navigation, siteConfig } from "@/config"
+import { useDialog } from "@/hooks"
+import { Bug, MenuIcon } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Button, buttonVariants } from "@/components/ui/button"
+import {
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { Separator } from "@/components/ui/separator"
+import { Icons } from "@/components/icons"
+
+export const SiteMenu = () => {
+  const [open, setOpen] = useDialog()
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen} shouldScaleBackground={false}>
+      <DrawerTrigger asChild>
+        <Button size="icon" variant="outline">
+          <MenuIcon className="size-4" />
+        </Button>
+      </DrawerTrigger>
+
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Menu</DrawerTitle>
+        </DrawerHeader>
+
+        <Accordion type="multiple" className="px-2">
+          {navigation.items.map((item) =>
+            item.items ? (
+              <MultipleMenuItem key={item.title} {...item} />
+            ) : (
+              <MenuItem key={item.title} {...item} />
+            )
+          )}
+        </Accordion>
+
+        <Separator className="mt-4" />
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+const MenuItem = ({ title, href, icon: Icon }: NavItem) => {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        buttonVariants({ variant: "ghost" }),
+        "w-full justify-between hover:no-underline"
+      )}
+    >
+      <div className={cn("flex items-center justify-start")}>
+        <Icon className="mr-2 size-4" />
+        {title}
+      </div>
+    </Link>
+  )
+}
+
+const MultipleMenuItem = ({ title, items, icon: Icon }: NavItem) => {
+  return (
+    <AccordionItem className="border-b-0" value={title}>
+      <AccordionTrigger
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "w-full justify-between hover:no-underline"
+        )}
+      >
+        <div className={cn("flex items-center justify-start")}>
+          <Icon className="mr-2 size-4" />
+          {title}
+        </div>
+      </AccordionTrigger>
+      <AccordionContent className="pl-4">
+        {items?.map((item) => (
+          <MenuItem key={item.title} {...item} />
+        ))}
+      </AccordionContent>
+    </AccordionItem>
+  )
+}
